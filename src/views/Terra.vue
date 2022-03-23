190f0778f14e4237c77c59a1b968409377dae79a
José Carlos <etijcvivi@gmail.com>
2022-03-23 19:18:11 -0300

    Projeto Planetas actions Terra fase 1

--- src/views/Terra.vue
+++ src/views/Terra.vue
@@ -10,18 +10,29 @@
         <div class="descricao">
             <div class="infoTerra">
                 <h1>Terra</h1>
-                <p>
-                Terceiro planeta a partir do Sol e o único planeta conhecido a abrigar vida. 
-                Cerca de 29,2% da superfície da Terra é terra, com os restantes 70,8% cobertos por água. 
-                A distância da Terra ao Sol, as propriedades físicas e a história geológica permitiram 
-                que a vida evoluísse e prosperasse. <br><br><a href="https://pt.wikipedia.org/wiki/Terra#:~:text=A%20Terra%20%C3%A9%20o%20terceiro,como%20Mundo%20ou%20Planeta%20Azul.">Wikipedia</a>
+                <p v-if="mostraVisao">
+                    Terceiro planeta a partir do Sol e o único planeta conhecido a abrigar vida. 
+                    Cerca de 29,2% da superfície da Terra é terra, com os restantes 70,8% cobertos por água. 
+                    A distância da Terra ao Sol, as propriedades físicas e a história geológica permitiram 
+                    que a vida evoluísse e prosperasse. <br><br><a href="https://pt.wikipedia.org/wiki/Terra#:~:text=A%20Terra%20%C3%A9%20o%20terceiro,como%20Mundo%20ou%20Planeta%20Azul.">Wikipedia</a>
+                </p>
+
+                <p v-if="mostraEstrutura">
+                    O interior da Terra, como o de outros planetas terrestres, é dividido em camadas por suas 
+                    propriedades químicas ou físicas (reológicas). A camada externa é uma crosta sólida de 
+                    silicato quimicamente distinta, que é sustentada por um manto sólido altamente viscoso. 
+                    <br><br><a href="https://pt.wikipedia.org/wiki/Terra#:~:text=A%20Terra%20%C3%A9%20o%20terceiro,como%20Mundo%20ou%20Planeta%20Azul.">Wikipedia</a>
                 </p>
             </div>
 
             <div class="botoesDescricao">
-                    <button type="button" value="">Visão Geral</button>
-                    <button type="button" value="">Estrutura Interna</button>
-                    <button type="button" value="">Geologia da Superfície</button>
+                    <button type="button" value="" class="btn" 
+                    :class="{ active: mostraVisao}" @click="verVisao">Visão Geral</button>
+
+                    <button type="button" value="" class="btn"
+                    :class="{ active: mostraEstrutura }" @click="verEstrutura">Estrutura Interna</button>
+
+                    <button type="button" value="" class="btn">Geologia da Superfície</button>
             </div>
         </div>
     </div>
@@ -39,7 +50,7 @@ export default {
     },
     methods: {
         
-        verVisão() {
+        verVisao() {
             this.mostraVisao = true;
             this.mostraEstrutura = false;
             this.mostraGeologia = false;
