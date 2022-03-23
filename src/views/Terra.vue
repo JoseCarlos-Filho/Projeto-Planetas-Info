a40f457b3690b7c3485d8ec899ed90bb9589b3c1
José Carlos <etijcvivi@gmail.com>
2022-03-23 20:11:11 -0300

    Projeto Planetas actions Terra fase 2

--- src/views/Terra.vue
+++ src/views/Terra.vue
@@ -3,13 +3,16 @@
         
         <div class="planetaTerra">
             <div>
-                <img class="terra" src="../assets/Terra-bg.png">
+                <img class="terra" src="../assets/Terra-bg.png" v-if="mostraVisao">
+                <img class="terra" src="../assets/estrutura-da-terra-bg.png" v-if="mostraEstrutura">
+                <img class="terra" src="../assets/Terra-bg.png" v-if="mostraGeologia">
             </div>   
         </div>
 
         <div class="descricao">
             <div class="infoTerra">
                 <h1>Terra</h1>
+                <!-- O atributo "mostra###" é verdadeiro quando selecionado no botão "btn" correspondente.-->
                 <p v-if="mostraVisao">
                     Terceiro planeta a partir do Sol e o único planeta conhecido a abrigar vida. 
                     Cerca de 29,2% da superfície da Terra é terra, com os restantes 70,8% cobertos por água. 
@@ -23,16 +26,29 @@
                     silicato quimicamente distinta, que é sustentada por um manto sólido altamente viscoso. 
                     <br><br><a href="https://pt.wikipedia.org/wiki/Terra#:~:text=A%20Terra%20%C3%A9%20o%20terceiro,como%20Mundo%20ou%20Planeta%20Azul.">Wikipedia</a>
                 </p>
+
+                <p v-if="mostraGeologia">
+                    A superfície total da Terra é de cerca de 510 milhões de km2. 
+                    A crosta continental é constituída por material de menor densidade como as rochas ígneas granito e andesito. 
+                    Menos comum é o basalto, uma rocha vulcânica mais densa que é o principal constituinte dos fundos oceânicos. 
+                    <br><br><a href="https://pt.wikipedia.org/wiki/Terra#:~:text=A%20Terra%20%C3%A9%20o%20terceiro,como%20Mundo%20ou%20Planeta%20Azul.">Wikipedia</a>
+                </p> 
+
             </div>
 
+            <!-- Botões que pega ação do @click seleciona o atributo "mostra###" e passa como parametro 
+                para a função "ver###" mostranto o conteudo referente ao valor booleano setado.-->
             <div class="botoesDescricao">
+
                     <button type="button" value="" class="btn" 
                     :class="{ active: mostraVisao}" @click="verVisao">Visão Geral</button>
 
                     <button type="button" value="" class="btn"
                     :class="{ active: mostraEstrutura }" @click="verEstrutura">Estrutura Interna</button>
 
-                    <button type="button" value="" class="btn">Geologia da Superfície</button>
+                    <button type="button" value="" class="btn"
+                    :class="{ active: mostraGeologia }" @click="verGeologia">Geologia da Superfície</button>
+
             </div>
         </div>
     </div>
