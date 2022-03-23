bcc5090c4ef780e9a03dee3be26f3ca61469b68f
José Carlos <etijcvivi@gmail.com>
2022-03-23 18:06:13 -0300

    Projeto Planetas Config View Terra

--- src/views/Terra.vue
+++ src/views/Terra.vue
@@ -1,7 +1,34 @@
 <template>
-    <div class="texto">
-        <h1>Terra</h1>    
+    <div class="infoPlaneta">
+        
+        <div class="planetaTerra">
+            <div class="imagemTerra">
+                <img class="terra" src="../assets/Terra-bg.png">
+            </div>   
+        </div>
+
+        <div class="descricao">
+            <div class="infoTerra">
+                <h1>Terra</h1>
+                <p>
+                Terceiro planeta a partir do Sol e o único planeta conhecido a abrigar vida. 
+                Cerca de 29,2% da superfície da Terra é terra, com os restantes 70,8% cobertos por água. 
+                A distância da Terra ao Sol, as propriedades físicas e a história geológica permitiram 
+                que a vida evoluísse e prosperasse.
+                </p>
+                <a href="https://pt.wikipedia.org/wiki/Terra#:~:text=A%20Terra%20%C3%A9%20o%20terceiro,como%20Mundo%20ou%20Planeta%20Azul.">Wikipedia</a>
+            </div>
+
+            <div class="botoesDescricao">
+                <ul>
+                    <li><button type="button" value="">Visão Geral</button></li>
+                    <li><button type="button" value="">Estrutura Interna</button></li>
+                    <li><button type="button" value="">Geologia da Superfície</button></li>
+                </ul>
+            </div>
+        </div>
     </div>
+
 </template>
 
 <script>
@@ -9,16 +36,51 @@
 </script>
 
 <style lang="scss">
-    .texto {
+    .infoPlaneta {
         display: flex;
-    }
-
-    h1 {
-        font-size: 50px;
-        font-weight: 700;
+        justify-content: center;
         align-items: center;
-        color: white;
-        text-align: center;
+
+    }
+    
+    .planetaTerra {
+        margin-top: 8em;
+    }
+    
+    .terra{
+        height: 350px;
+        width: 400px;
     }
 
+    .descricao {
+        height:400px;
+        width: 400px;
+    }
+
+    .botoesDescricao {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+
+        
+    }
+
+    .botoesDescricao li {
+        list-style: none;
+        margin: 0px 10px;
+    }
+
+    .descricao a {
+        text-decoration: none;
+        color: gray;
+        border-bottom: 1px solid gray;
+        cursor: pointer;
+
+    }
+
+    button {
+        margin: 10px;
+        padding: 5px 40px;  
+        cursor: pointer;      
+    }
 </style>
