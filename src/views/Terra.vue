53991bfc0aabbf8ebc979dde2d783d5f1dc55849
José Carlos <etijcvivi@gmail.com>
2022-03-23 18:57:29 -0300

    Projeto Planetas Config actions Terra

--- src/views/Terra.vue
+++ src/views/Terra.vue
@@ -2,7 +2,7 @@
     <div class="infoPlaneta">
         
         <div class="planetaTerra">
-            <div class="imagemTerra">
+            <div>
                 <img class="terra" src="../assets/Terra-bg.png">
             </div>   
         </div>
@@ -14,17 +14,14 @@
                 Terceiro planeta a partir do Sol e o único planeta conhecido a abrigar vida. 
                 Cerca de 29,2% da superfície da Terra é terra, com os restantes 70,8% cobertos por água. 
                 A distância da Terra ao Sol, as propriedades físicas e a história geológica permitiram 
-                que a vida evoluísse e prosperasse.
+                que a vida evoluísse e prosperasse. <br><br><a href="https://pt.wikipedia.org/wiki/Terra#:~:text=A%20Terra%20%C3%A9%20o%20terceiro,como%20Mundo%20ou%20Planeta%20Azul.">Wikipedia</a>
                 </p>
-                <a href="https://pt.wikipedia.org/wiki/Terra#:~:text=A%20Terra%20%C3%A9%20o%20terceiro,como%20Mundo%20ou%20Planeta%20Azul.">Wikipedia</a>
             </div>
 
             <div class="botoesDescricao">
-                <ul>
-                    <li><button type="button" value="">Visão Geral</button></li>
-                    <li><button type="button" value="">Estrutura Interna</button></li>
-                    <li><button type="button" value="">Geologia da Superfície</button></li>
-                </ul>
+                    <button type="button" value="">Visão Geral</button>
+                    <button type="button" value="">Estrutura Interna</button>
+                    <button type="button" value="">Geologia da Superfície</button>
             </div>
         </div>
     </div>
@@ -32,7 +29,36 @@
 </template>
 
 <script>
+export default {
+    data(){
+        return {
+            mostraVisao: true,
+            mostraEstrutura: false,
+            mostraGeologia: false,
+        };
+    },
+    methods: {
+        
+        verVisão() {
+            this.mostraVisao = true;
+            this.mostraEstrutura = false;
+            this.mostraGeologia = false;
+        },
 
+        verEstrutura() {
+            this.mostraVisao = false;
+            this.mostraEstrutura = true;
+            this.mostraGeologia = false;
+        },
+
+        verGeologia() {
+            this.mostraVisao = false;
+            this.mostraEstrutura = false;
+            this.mostraGeologia = true;
+        },
+
+    },
+}
 </script>
 
 <style lang="scss">
@@ -57,30 +83,42 @@
         width: 400px;
     }
 
+    .descricao h1, p {
+        text-align: left;
+    }
+
+    .descricao .botoesDescricao {
+        text-align: left;
+
+    }
+
     .botoesDescricao {
         display: flex;
-        align-items: center;
         flex-direction: column;
-
         
     }
 
-    .botoesDescricao li {
-        list-style: none;
-        margin: 0px 10px;
-    }
 
     .descricao a {
         text-decoration: none;
-        color: gray;
-        border-bottom: 1px solid gray;
+        color: black;
+        font-weight: 700;
+        border-bottom: 1px solid black;
         cursor: pointer;
 
     }
 
     button {
-        margin: 10px;
-        padding: 5px 40px;  
-        cursor: pointer;      
+        display: flex;
+        margin: 10px 0px;
+        padding: 10px 20px;
+        align-items: center;
+        border: 1px solid #777;
+        cursor: pointer;
+        text-align: center;
+
+        // margin: 10px;
+        // padding: 5px 40px;  
+        // cursor: pointer;      
     }
 </style>
