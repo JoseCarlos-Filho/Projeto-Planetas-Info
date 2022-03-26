b2367864f316a26e23b71a8342602d9ed47dcc9a
José Carlos <etijcvivi@gmail.com>
2022-03-26 18:07:12 -0300

    Projeto Planetas Page Venus e Marte

--- src/views/Marte.vue
+++ src/views/Marte.vue
@@ -1,24 +1,107 @@
 <template>
-    <div class="texto">
-        <h1>Marte</h1>   
+    <div class="infoPlaneta">
+         <informacaoPlaneta
+
+            :srcPlaneta = imgUrl 
+
+            :clName =  className
+        /> 
+        
+
+        <div class="descricao">
+            <conteudoPlaneta
+                titulo = "Marte"
+                :content = conteudo
+                wikipedia = "https:\/\/pt.wikipedia.org/wiki/Marte_(planeta)"
+            />
+
+            <!-- Botões que pega ação do @click seleciona o atributo "mostra###" e passa como parametro 
+                para a função "ver###" mostranto o conteudo referente ao valor booleano setado.-->
+            <div class="botoesDescricao">
+
+                    <button type="button" value="" class="btn" 
+                    :class="{ active: mostraVisao}" @click="verVisao">Visão Geral</button>
+
+                    <button type="button" value="" class="btn"
+                    :class="{ active: mostraEstrutura }" @click="verEstrutura">Estrutura Interna</button>
+
+                    <button type="button" value="" class="btn"
+                    :class="{ active: mostraGeologia }" @click="verGeologia">Geologia da Superfície</button>
+
+            </div>
+        </div>
     </div>
+
 </template>
 
 <script>
+import informacaoPlaneta from '../components/acoes.vue'
+import conteudoPlaneta from '../components/textos.vue'
+import {ref} from 'vue'
+export default {
+    data(){
+        return {
+            imgUrl: "marte/Marte-bg.png",
+            className: "imgPlaneta",
+            conteudo: "Marte é o quarto planeta a partir do Sol e o segundo menor planeta do Sistema Solar,\n" + 
+                      "sendo maior que apenas Mercúrio. Em inglês, Marte carrega o nome do deus romano da guerra\n" + 
+                      "e é muitas vezes referido como o 'Planeta Vermelho.'",
 
+            mostraVisao: ref(true),
+            mostraEstrutura: ref(false),
+            mostraGeologia: ref(false),
+        };
+
+        
+    },
+    components: {
+        informacaoPlaneta,
+        conteudoPlaneta
+    },
+
+    methods: {
+        
+        verVisao() {
+            this.imgUrl = "marte/Marte-bg.png"
+            this.className = "imgPlaneta"
+            this.conteudo = "Marte é o quarto planeta a partir do Sol e o segundo menor planeta do Sistema Solar,\n" + 
+                            "sendo maior que apenas Mercúrio. Em inglês, Marte carrega o nome do deus romano da guerra\n" + 
+                            "e é muitas vezes referido como o 'Planeta Vermelho.'"
+
+            this.mostraVisao = ref(true)
+            this.mostraEstrutura = ref(false)
+            this.mostraGeologia = ref(false)
+        },
+
+        verEstrutura() {
+            this.imgUrl = "marte/marte-estrutura-bg.png"
+            this.className = "imgPlaneta"
+            this.conteudo = "Como a Terra, Marte se diferenciou em um núcleo metálico denso sobreposto por materiais menos densos.\n" + 
+                            "Os cientistas determinaram inicialmente que o núcleo é pelo menos parcialmente líquido.\n" + 
+                            "Os modelos atuais de seu interior implicam um núcleo consistindo principalmente de ferro e níquel\n" + 
+                            "com cerca de 16 a 17% de enxofre."
+
+            this.mostraVisao = ref(false)
+            this.mostraEstrutura = ref(true)
+            this.mostraGeologia = ref(false)
+        },
+
+        verGeologia() {
+            this.imgUrl = "marte/marte-superficie-bg.jpg"
+            this.className = "imgPlanetaS"
+            this.conteudo =  "Marte é um planeta terrestre cuja superfície consiste em minerais contendo silício e oxigênio,\n" + 
+                             "metais e outros elementos que normalmente compõem a rocha. A superfície é composta principalmente\n" + 
+                             "de basalto toleítico, embora as partes sejam mais ricas em sílica do que o basalto típico."
+
+            this.mostraVisao = ref(false)
+            this.mostraEstrutura = ref(false)
+            this.mostraGeologia = ref(true)
+        },
+
+    },
+}
 </script>
 
 <style lang="scss">
-    .texto {
-        display: flex;
-    }
-
-    h1 {
-        font-size: 50px;
-        font-weight: 700;
-        align-items: center;
-        color: white;
-        text-align: center;
-    }
-
+    @import "../scss/style_Planetas.scss";
 </style>
--- src/views/Venus.vue
+++ src/views/Venus.vue
@@ -1,23 +1,108 @@
 <template>
-    <div class="texto">
-        <h1>Venus</h1>   
+    <div class="infoPlaneta">
+         <informacaoPlaneta
+
+            :srcPlaneta = imgUrl 
+
+            :clName =  className
+        /> 
+        
+
+        <div class="descricao">
+            <conteudoPlaneta
+                titulo = "Vênus"
+                :content = conteudo
+                wikipedia = "https:\/\/pt.wikipedia.org/wiki/V%C3%A9nus_(planeta)"
+            />
+
+            <!-- Botões que pega ação do @click seleciona o atributo "mostra###" e passa como parametro 
+                para a função "ver###" mostranto o conteudo referente ao valor booleano setado.-->
+            <div class="botoesDescricao">
+
+                    <button type="button" value="" class="btn" 
+                    :class="{ active: mostraVisao}" @click="verVisao">Visão Geral</button>
+
+                    <button type="button" value="" class="btn"
+                    :class="{ active: mostraEstrutura }" @click="verEstrutura">Estrutura Interna</button>
+
+                    <button type="button" value="" class="btn"
+                    :class="{ active: mostraGeologia }" @click="verGeologia">Geologia da Superfície</button>
+
+            </div>
+        </div>
     </div>
+
 </template>
 
 <script>
+import informacaoPlaneta from '../components/acoes.vue'
+import conteudoPlaneta from '../components/textos.vue'
+import {ref} from 'vue'
+export default {
+    data(){
+        return {
+            imgUrl: "venus/venus-bg.png",
+            className: "imgPlaneta",
+            conteudo: "Vênus é o segundo planeta a partir do Sol. É nomeado após a deusa romana do amor e da beleza.\n" + 
+                      "Como o objeto natural mais brilhante no céu noturno da Terra depois da Lua,\n" + 
+                      "Vênus pode lançar sombras e pode ser, em raras ocasiões, visível a olho nu em plena luz do dia.",
 
+            mostraVisao: ref(true),
+            mostraEstrutura: ref(false),
+            mostraGeologia: ref(false),
+        };
+
+        
+    },
+    components: {
+        informacaoPlaneta,
+        conteudoPlaneta
+    },
+
+    methods: {
+        
+        verVisao() {
+            this.imgUrl = "venus/venus-bg.png"
+            this.className = "imgPlaneta"
+            this.conteudo = "Vênus é o segundo planeta a partir do Sol. É nomeado após a deusa romana do amor e da beleza.\n" + 
+                            "Como o objeto natural mais brilhante no céu noturno da Terra depois da Lua,\n" + 
+                            "Vênus pode lançar sombras e pode ser, em raras ocasiões, visível a olho nu em plena luz do dia."
+
+            this.mostraVisao = ref(true)
+            this.mostraEstrutura = ref(false)
+            this.mostraGeologia = ref(false)
+        },
+
+        verEstrutura() {
+            this.imgUrl = "venus/venus-estrutura-bg.png"
+            this.className = "imgPlaneta"
+            this.conteudo = "A semelhança em tamanho e densidade entre Vênus e a Terra sugere que eles compartilham uma\n" + 
+                            "estrutura interna semelhante: um núcleo, manto e crosta. Como o da Terra, o núcleo venusiano\n" + 
+                            "é provavelmente pelo menos parcialmente líquido porque os dois planetas estão esfriando\n" + 
+                            "aproximadamente na mesma taxa."
+
+            this.mostraVisao = ref(false)
+            this.mostraEstrutura = ref(true)
+            this.mostraGeologia = ref(false)
+        },
+
+        verGeologia() {
+            this.imgUrl = "venus/venus-superficie-bg.jpeg"
+            this.className = "imgPlanetaS"
+            this.conteudo =  "Grande parte da superfície venusiana parece ter sido moldada pela atividade vulcânica.\n" + 
+                             "Vênus tem várias vezes mais vulcões que a Terra, e tem 167 grandes vulcões\n" + 
+                             "com mais de 100 km (60 milhas) de diâmetro. O único complexo vulcânico deste tamanho na\n" + 
+                             "Terra é a Grande Ilha do Havaí."
+
+            this.mostraVisao = ref(false)
+            this.mostraEstrutura = ref(false)
+            this.mostraGeologia = ref(true)
+        },
+
+    },
+}
 </script>
 
 <style lang="scss">
-    .texto {
-        display: flex;
-    }
-
-    h1 {
-        font-size: 50px;
-        font-weight: 700;
-        align-items: center;
-        color: white;
-        text-align: center;
-    }
+    @import "../scss/style_Planetas.scss";
 </style>
