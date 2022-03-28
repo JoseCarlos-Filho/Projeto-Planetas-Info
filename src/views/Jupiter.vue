3dd2d1c65ee326ef631cc7d632189b944a920a5b
José Carlos <etijcvivi@gmail.com>
2022-03-28 19:04:20 -0300

    Projeto Planetas routers scss 6

--- src/views/Jupiter.vue
+++ src/views/Jupiter.vue
@@ -103,5 +103,5 @@ export default {
 </script>
 
 <style lang="scss">
-    @import '~@/scss/style_Planetas.scss';
+   @import "../scss/style_Planetas.scss";
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,25 @@
 module.exports = {
     css: {
         loaderOptions: {
-            scss: {
-                // additionalData: ` 
-                // @import "@/scss/variaveis_cores.scss";
-                // @import "~@/scss/style_Planetas.scss";
-                // @import "~@/scss/style_Principal.scss";
-                // `
-                prependData: ` 
-                @import "@/scss/style_Planetas.scss";
-                @import "@/scss/style_Principal.scss";
-                `
+            sass: {
+                data: ` 
+                    @import "@/scss/variaveis_cores.scss";
+                    @import "@/scss/style_Planetas.scss";
+                    @import "@/scss/style_Principal.scss";
+                    `
             }
+            // scss: {
+            //     additionalData: ` 
+            //     @import "@/scss/variaveis_cores.scss";
+            //     @import "~@/scss/style_Planetas.scss";
+            //     @import "~@/scss/style_Principal.scss";
+            //     `
+            //     prependData: ` 
+            //     @import "~@/scss/variaveis_cores.scss";
+            //     @import "@/scss/style_Planetas.scss";
+            //     @import "@/scss/style_Principal.scss";
+            //     `
+            // }
         }
     }
 }
