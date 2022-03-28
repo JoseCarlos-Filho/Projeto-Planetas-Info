5186005c3489b532ebf1407bf95d7b7c61d72ff2
José Carlos <etijcvivi@gmail.com>
2022-03-28 18:29:29 -0300

    Projeto Planetas routers scss 4

--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,16 @@ module.exports = {
     css: {
         loaderOptions: {
             scss: {
-                additionalData: ` 
-                @import "~@/src/scss/variaveis_cores.scss";
-                @import "~@/scss/style_Planetas.scss";
-                @import "~@/scss/style_Principal.scss";
-                `
-                // prependData: ` 
-                // @import "scss/variaveis_cores.scss";
-                // @import "@/scss/style_Planetas.scss";
-                // @import "@/scss/style_Principal.scss";
+                // additionalData: ` 
+                // @import "@/scss/variaveis_cores.scss";
+                // @import "~@/scss/style_Planetas.scss";
+                // @import "~@/scss/style_Principal.scss";
                 // `
+                prependData: ` 
+                @import "./src/scss/variaveis_cores.scss";
+                @import "@/scss/style_Planetas.scss";
+                @import "@/scss/style_Principal.scss";
+                `
             }
         }
     }
