6a521c64ba377a568a0eb2d794b002b8e156a676
José Carlos <etijcvivi@gmail.com>
2022-05-31 22:44:48 -0300

    atualização do comunicação do SCSS com a aplicação:  Vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
-// const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-    // transpileDependencies: true,
     css: {
         loaderOptions: {
             scss: {
@@ -15,18 +13,3 @@ module.exports = {
     }
 };
 
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true,
-//   css: {
-//     loaderOptions: {
-//       scss: {
-//         additionalData: `
-//           @import '/src/scss/variaveis_cores.scss';
-//           @import '/src/scss/style_Planetas.scss';
-//           @import '/src/scss/style_Principal.scss';
-//         `
-//       }
-//     }
-//   }
-// })
