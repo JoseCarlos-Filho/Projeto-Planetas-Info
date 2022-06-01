c91092b5b17a432304822b9c1f8e8fd2b6c6bd30
José Carlos <etijcvivi@gmail.com>
2022-05-31 22:18:53 -0300

    atualização do sass-loader

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// createApp(App).use(router).mount('#app')
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,32 @@
+// const { defineConfig } = require('@vue/cli-service')
 module.exports = {
+    // transpileDependencies: true,
     css: {
         loaderOptions: {
             scss: {
-                    prependData: ` 
-                        @import "./src/scss/variaveis_cores.scss";
-                        @import "@/scss/style_Planetas.scss";
-                        @import "@/scss/style_Principal.scss";
+                    // prependData:
+                    additionalData: `  
+                        @import "~@/variaveis_cores.scss";
+                        @import "~@/style_Planetas.scss";
+                        @import "~@/style_Principal.scss";
                         `
-            }
+            },
         }
     }
 }
+
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   css: {
+//     loaderOptions: {
+//       scss: {
+//         additionalData: `
+//           @import '/src/scss/variaveis_cores.scss';
+//           @import '/src/scss/style_Planetas.scss';
+//           @import '/src/scss/style_Principal.scss';
+//         `
+//       }
+//     }
+//   }
+// })
